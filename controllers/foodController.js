7942d84242225e087b4ebf4c01d7f5743069a4dc
CoVanAn <covanan632003@gmail.com>
2024-05-02 17:24:17 +0700

    backend

---
+++ controllers/foodController.js
@@ -0,0 +1,54 @@
+import foodModel from "../models/foodModel.js";
+import fs from "fs";
+
+
+// add a new food item
+const addFood = async (req, res) => {
+
+    
+    let image_filename = `${req.file.filename}`;
+
+    const food = new foodModel({
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        image: image_filename,
+        category: req.body.category
+    });
+
+    try{
+        await food.save();
+        res.json({success: 'Food added successfully'});
+    } catch (error) {
+        console.log(error);
+        res.json({ message: "error"});
+    }
+}
+
+// all food items
+const listfood = async (req, res) => {
+    try {
+        const foods = await foodModel.find({});
+        res.json(foods);
+    } catch (error) {
+        console.log(error);
+        res.json({ message: "error"});
+    }
+}
+
+// remove food item
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlinkSync(`uploads/${food.image}`, () => {});
+        await foodModel.findOneAndDelete(req.body.id);
+        res.json({success: 'Food removed successfully'});
+    }
+    catch (error) {
+        console.log(error);
+        res.json({ message: "error"});
+    }
+}
+
+
+export {addFood, listfood, removeFood}
